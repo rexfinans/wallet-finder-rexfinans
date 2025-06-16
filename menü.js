fd429c43e5c18d30f76ad06c5eea48656518e352
REX <65308553+rexfinans@users.noreply.github.com>
2025-06-16 15:10:09 +0300

    Delete menü.js

--- menü.js
+++
@@ -1,8 +0,0 @@
-// menü.js
-const currentPage = location.pathname.split("/").pop();
-
-document.querySelectorAll(".menu-item").forEach((item) => {
-  if (item.getAttribute("href") === currentPage) {
-    item.classList.add("active");
-  }
-});
